44fb7d6ea8f39dfa169e05279223fa8e619a23c1
Tom Gooding <tom.pudding@gmail.com>
2012-06-02 22:43:00 +0100

    I'm so far from working code I can't even see land

--- ui.py
+++ ui.py
@@ -75,6 +75,10 @@ class UIElement(object):
         for q in self.children:
             child.Enable()
 
+    def Delete(self):
+        for child in self.children:
+            child.Delete()
+
     def MakeSelectable(self):
         self.on = True
         for child in self.children:
@@ -96,24 +100,26 @@ class Box(UIElement):
         self.colour = colour
         self.unselectable_colour = tuple(component*0.6 for component in self.colour)
         self.quad.SetColour(self.colour)
-        self.quad.SetVertices(self.bottom_left,
-                              self.top_right,
+        self.quad.SetVertices(self.absolute.bottom_left,
+                              self.absolute.top_right,
+                              utils.ui_level)
+
+    def UpdatePosition(self):
+        super(Box,self).UpdatePosition()
+        self.quad.SetVertices(self.absolute.bottom_left,
+                              self.absolute.top_right,
                               utils.ui_level)
 
     def Delete(self):
         self.quad.Delete()
-        for child in self.children:
-            child.Delete()
         
     def Disable(self):
+        super(Box,self).Disable()
         self.quad.Disable()
-        for child in self.children:
-            child.Disable()
 
     def Enable(self):
+        super(Box,self).Enable()
         self.quad.Enable()
-        for child in self.children:
-            child.Enable()
 
     def MakeSelectable(self):
         super(Box,self).MakeSelectable()
@@ -130,7 +136,6 @@ class Box(UIElement):
 class TextBox(UIElement):
     """ A Screen-relative text box wraps text to a given size """
     def __init__(self,parent,bl,tr,text,scale,colour = None):
-        self.parent      = parent
         super(TextBox,self).__init__(parent,bl,tr)
         self.text        = text
         self.scale       = scale
@@ -142,6 +147,7 @@ class TextBox(UIElement):
         self.Position(self.bottom_left,self.scale,self.colour)
 
     def Position(self,pos,scale,colour = None):
+        """Draw the text at the given location and size. Maybe colour too"""
         #set up the position for the characters
         self.pos = pos
         self.absolute.bottom_left = self.GetAbsoluteInParent(pos)
@@ -172,27 +178,38 @@ class TextBox(UIElement):
         height = max([q.height for q in self.quads])
         self.UpdatePosition()
 
+    def UpdatePosition(self):
+        """Called by the parent to tell us we need to recalculate our absolute position"""
+        super(Box,self).UpdatePosition()
+        self.Position(pos,self.scale,self.colour)
+
     def SetPos(self,pos):
+        """Called by the user to update our position directly"""
         self.SetBounds(pos,pos + self.size)
         self.Position(pos,self.scale,self.colour)
 
     def Delete(self):
+        """We're done; pack up and go home!"""
+        super(TextBox,self).Delete()
         for quad in self.quads:
             quad.Delete()
 
     def SetText(self,text,colour = None):
+        """Update the text"""
         self.Delete()
         self.text = text
         self.quads = [self.textmanager.Letter(char,self.text_type) for char in self.text]
         self.Position(self.pos,self.scale,colour)
 
     def Disable(self):
-        super(Disable,self).Disable()
+        """Don't draw for a while, maybe we'll need you again"""
+        super(TextBox,self).Disable()
         for q in self.quads:
             q.Disable()
 
     def Enable(self):
-        super(Disable,self).Enable()
+        """Alright, you're back on the team!"""
+        super(TextBox,self).Enable()
         for q in self.quads:
             q.Enable()
 
@@ -210,6 +227,10 @@ class TextBoxButton(TextBox):
         
     def Position(self,pos,scale,colour = None):
         super(TextBoxButton,self).Position(pos,scale,colour)
+        self.SetVertices()
+
+    def UpdatePosition(self):
+        super(TextBoxButton,self).UpdatePosition()
         self.SetVertices()
 
     def SetVertices(self):
@@ -299,24 +320,17 @@ class TexturedButton(TextButton):
         for i in xrange(4):
             self.hover_quads[i].Disable()
 
-class ButtonList(object):
+class ButtonList(UIElement):
     """
     UI Element for showing a list of buttons. Right now it's very basic, does not actually 
     inherit from UIElement(as it doesn't accept clicks itself), and doesn't really do anything
 
     Just a placeholder really in case we need scrollbars at some point
     """
-    
-    def __init__(self,pos,l = []):
-        self.buttons  = []
-        self.top_left = pos
-        self.itemheight = 0.04*gamedata.screen.y
-        for item in l:
-            self.AddButton(l)
 
-    def AddButton(self,button):
-        button.text.SetPos(self.top_left - Point(0,self.itemheight*len(self.buttons)))
-        self.buttons.append(button)
+    def AddElement(self,element):
+        element.SetPos(self.top_left - Point(0,self.itemheight*len(self.buttons)))
+        self.children.append(button)
 
     def Enable(self,tiles):
         for button in self.buttons:
@@ -330,38 +344,3 @@ class ButtonList(object):
 
     def __getitem__(self,index):
         return self.buttons[index]
-
-
-class ControlBox(UIElement):
-    def __init__(self,bl,tr,colour):
-        self.bl       = bl
-        self.tr       = tr
-        self.size     = tr-bl
-        self.colour   = colour
-        self.ui_box   = Box(bl,
-                              tr,
-                              colour)
-        self.buttons  = {}
-        self.elements = [self.ui_box]
-
-    def AddButton(self,text,pos,callback,size = None):
-        #FIXME: Sort out the arguments here, there's really no need to duplicate code I'm sure
-        if None == size:
-            button = TextButton(text,self.bl+(self.size*pos),callback=callback)
-        else:
-            button = TextButton(text,self.bl+(self.size*pos),size=size,callback=callback)
-        button.level = 2
-        self.buttons[text] = button
-        self.elements.append(button)
-
-    def Register(self,tiles,level):
-        for element in self.elements:
-            tiles.RegisterUIElement(element,level+0.1 if isinstance(element,TextButton) else level)
-        
-    def MakeSelectable(self):
-        for element in self.elements:
-            element.MakeSelectable()
-
-    def MakeUnselectable(self):
-        for element in self.elements:
-            element.MakeUnselectable()
